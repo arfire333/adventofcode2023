7ce2eab8c8c993d64a360fbc67b3c553d7f8f928
Ar Fire <72059150+arfire333@users.noreply.github.com>
2023-12-09 14:02:26 +0000

    OASIS Analysis... but what about the hang glider?
      - Also updated getNumbers to handle negatives.

---
+++ 2023day09/2023day09_2.cpp
@@ -0,0 +1,56 @@
+#include <libaoc.h>
+#include <stdint.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void getData(istream& in, vector<vector<vector<int64_t>>>& data) {
+  string s;
+
+  while (getline(in, s) && s.size()) {
+    vector<vector<int64_t>> row;
+    row.push_back(aoc::getNumbers(s));
+    data.push_back(row);
+  }
+}
+
+#ifndef EXCLUDE_MAIN
+int main(int argc, char* argv[]) {
+  string filename = aoc::dataFilename("2023day09", "user.input");
+  ifstream in(filename);
+  vector<vector<vector<int64_t>>> data;
+  getData(in, data);
+  int64_t sum = 0;
+  for (auto& history : data) {
+    int max = 1;
+    int S = history[0].size();
+    int row = 0;
+    do {
+      history.push_back(vector<int64_t>(S, 0));
+      auto& l = history[history.size() - 2];
+      auto& h = history[history.size() - 1];
+      max = 0;
+      row++;
+      for (int i = 1; i < S - row + 1; i++) {
+        h[i - 1] = l[i] - l[i - 1];
+        if (h[i - 1] != 0) {
+          max = h[i - 1];
+        }
+      };
+    } while (max != 0);
+    row--;
+    while (row > 0) {
+      history[row][S - row] = history[row][0] - history[row + 1][S - row - 1];
+      row--;
+    }
+    cout << history[0][0] - history[1][S - 1] << "\n";
+    sum += (history[0][0] - history[1][S - 1]);
+  }
+  cout << " sum=" << sum << "\n";
+
+  return 0;
+}
+#endif
---
+++ 2023day09/test_2023day09.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "2023day09.cpp"
+
+TEST(Test2023day09, Nothing) {
+  ASSERT_EQ(1, 1);
+}

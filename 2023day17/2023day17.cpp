166152d8fbec7d1f92d6dcd1b3f4f54803f3454b
Ar Fire <72059150+arfire333@users.noreply.github.com>
2023-12-22 02:03:18 +0000

    Part 1 is done...sort of.

--- 2023day17/2023day17.cpp
+++ 2023day17/2023day17.cpp
@@ -1,14 +1,17 @@
 #include <libaoc.h>
 #include <stdint.h>
+#include <algorithm>
 #include <climits>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <memory>
 #include <numeric>
 #include <queue>
 #include <set>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -35,13 +38,16 @@ struct pos_t {
   int col = 0;
   int time = 0;
   dir_t direction = dir_t::E;
-  int64_t heatloss = 0;
-  string history;
+  int64_t heatloss = INT_MAX;
 
   pos_t(int r, int c, int t, dir_t d) : row(r), col(c), time(t), direction(d) {}
 
-  friend bool operator<(const pos_t& l, const pos_t r) {
+  friend bool operator<(const pos_t& l, const pos_t& r) {
     return std::tie(l.row, l.col, l.time, l.direction) <
+           std::tie(r.row, r.col, r.time, r.direction);
+  }
+  friend bool operator==(const pos_t& l, const pos_t& r) {
+    return std::tie(l.row, l.col, l.time, l.direction) ==
            std::tie(r.row, r.col, r.time, r.direction);
   }
   friend ostream& operator<<(ostream& os, const pos_t& p) {
@@ -52,34 +58,48 @@ struct pos_t {
   }
 };
 
+class PosCompare {
+ public:
+  bool operator()(const pos_t& l, const pos_t& r) const {
+    return l.heatloss > r.heatloss;
+  }
+};
+bool priority_sort(const pos_t& l, const pos_t& r) {
+  return l.heatloss < r.heatloss;
+}
+
 int64_t plan(const vector<string>& city) {
-  stringstream ss;
+  const size_t CITY_LENGTH = city.size();
+  const size_t CITY_WIDTH = city[0].size();
   int64_t min_heatloss = INT_MAX;
   vector<dir_t> pos_dir = {dir_t::N, dir_t::E, dir_t::S, dir_t::W};
-  queue<pos_t> q;
-  map<pos_t, int64_t> visited;
-
-  pos_t start(0, 0, 0, dir_t::E);
-  q.push(start);
-  while (!q.empty()) {
-    auto curr = q.front();
-    q.pop();
-    ss.str("");
-
-    // Check if at end
-    if (curr.row == city.size() - 1 && curr.col == city[0].size() - 1) {
-      if (curr.heatloss < min_heatloss) {
-        cout << "End: " << curr.heatloss << " : " << curr.history << "\n";
-        min_heatloss = curr.heatloss;
+  vector<pos_t> grid;
+  map<pos_t, int64_t> dist;
+  map<pos_t, pos_t> prev;
+  set<pos_t> visited;
+  vector<pos_t> q;
+  pos_t INVALID(-1, -1, 0, dir_t::N);
+  for (auto r = 0; r < CITY_LENGTH; r++) {
+    for (auto c = 0; c < CITY_WIDTH; c++) {
+      for (auto t = 1; t <= 3; t++) {
+        for (auto d : pos_dir) {
+          q.emplace_back(r, c, t, d);
+        }
       }
     }
+  }
+  pos_t start(0, 0, 0, dir_t::E);
+  start.heatloss = 0;
+  q.emplace_back(start);
+  while (!q.empty()) {
+    sort(q.begin(), q.end(), priority_sort);
+    auto curr = *q.begin();
+    q.erase(q.begin());
+
     for (auto direction : pos_dir) {
-      ss.str("");
-      auto next = curr;
-      next.time++;
-      ss << curr.history;
+      int next_time = curr.time + 1;
       if (curr.direction != direction) {
-        next.time = 1;
+        next_time = 1;
       }
       // Can't reverse
       if (curr.direction == dir_t::N && direction == dir_t::S) {
@@ -94,44 +114,68 @@ int64_t plan(const vector<string>& city) {
       if (curr.direction == dir_t::E && direction == dir_t::W) {
         continue;
       }
-      if (next.time > 3) {
+      if (next_time > 3) {
         continue;  // Only here if same direction and had 3 moves in same
                    // direction
       }
-      next.direction = direction;
-      if (next.direction == dir_t::N) {
-        next.row--;
+      int next_row = curr.row;
+      int next_col = curr.col;
+      if (direction == dir_t::N) {
+        next_row--;
       }
-      if (next.direction == dir_t::E) {
-        next.col++;
+      if (direction == dir_t::E) {
+        next_col++;
       }
-      if (next.direction == dir_t::W) {
-        next.col--;
+      if (direction == dir_t::W) {
+        next_col--;
       }
-      if (next.direction == dir_t::S) {
-        next.row++;
+      if (direction == dir_t::S) {
+        next_row++;
       }
-      if (next.row < 0 || next.row >= city.size() || next.col < 0 ||
-          next.col >= city[0].size()) {
+      if (next_row < 0 || next_row >= CITY_LENGTH || next_col < 0 ||
+          next_col >= CITY_WIDTH) {
         continue;
       }
-      next.heatloss += city[next.row][next.col];
-      if (visited.count(next)) {
-        auto existing = visited.at(next);
-        if (existing < next.heatloss) {
-          continue;  // Already been there after that many hops so nothing to
-                     // learn
-        } else {
-          visited.erase(next);
+
+      int64_t next_heatloss = curr.heatloss + city[next_row][next_col];
+      pos_t next(next_row, next_col, next_time, direction);
+      next.heatloss = next_heatloss;
+
+      if (visited.find(next) != visited.end()) {
+        // already visited so don't do antyhing with it
+        continue;
+      }
+      auto it = dist.find(next);
+      bool update = false;
+      if (it == dist.end()) {
+        dist[next] = next.heatloss;
+        update = true;
+      } else {
+        auto current_next_heatloss = dist.at(next);
+        if (next.heatloss < current_next_heatloss) {
+          dist[next] = next.heatloss;
+          update = true;
         }
       }
-      ss << next;
+      if (update) {
+        for (auto& entry : q) {
+          if (next == entry) {
+            entry.heatloss = next.heatloss;
+          }
+        }
+      }
+    }
+    visited.insert(curr);
+  }
 
-      next.history = ss.str();
-      q.emplace(next);
-      visited.emplace(next, next.heatloss);
+  for (auto&& [f, s] : dist) {
+    if (f.row == CITY_LENGTH - 1 && f.col == CITY_WIDTH - 1 &&
+        s < min_heatloss) {
+      min_heatloss = s;
+      cout << f << " = " << f.heatloss << "\n";
     }
   }
+
   return min_heatloss;
 }
 #ifndef EXCLUDE_MAIN
